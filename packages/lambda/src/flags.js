3b90517b2370c9837077c52eec37e6e2c85cb897
Bogdan Klichuk <bogdan@gramble.com>
2017-12-01 04:08:29 +0200

    feat: added flags

--- packages/lambda/src/flags.js
+++ packages/lambda/src/flags.js
@@ -8,5 +8,17 @@ export default [
   // https://groups.google.com/a/chromium.org/d/msg/headless-dev/qqbZVZ2IwEw/Y95wJUh2AAAJ
   '--no-zygote', // helps avoid zombies
 
+  '--disable-background-networking',
+  '--disable-default-apps',
+  '--disable-extensions',
+  '--disable-sync',
+  '--disable-translate',
+  '--headless',
+  '--mute-audio',
+  '--window-size=1920,1080',
+  '--metrics-recording-only',
+  '--no-first-run',
+  '--safebrowsing-disable-auto-update',
+
   '--no-sandbox',
 ]
